33e443d6ea16e13442519a9795bf0b82b75a5066
Alexander Bukin <alex.bukin@hotmail.com>
2021-12-23 14:10:31 +0000

    Working on all possible game combinations

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 const players = Array.from(document.getElementsByClassName('player'));
 const winMsg = document.querySelector('.winning-message');
+const restart = document.querySelector('.restart');
 let currentPlayer;
+
+// Button to restart (reload) the game
+restart.addEventListener('click', () => location.reload());
 
 players.map(player => {
     player.addEventListener('click', () => {
@@ -14,23 +18,30 @@ players.map(player => {
     })
 })
 
+// All possible combinations of the game
 players.map(player => {
     for (let i = 0; i < boxes.length; i++){
         boxes[i].addEventListener('click', () => {
         if (player.classList.contains(player.innerText)) {    
             boxes[i].innerText = player.innerText;
         }
-        if (boxes[0].innerText === player.innerText && boxes[1].innerText === player.innerText && boxes[2].innerText === player.innerText){
-            winMsg.innerText = `Player ${player.innerText} wins`;
-        } else if (boxes[0].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[8].innerText === player.innerText) {
-            winMsg.innerText = `Player ${player.innerText} wins`;
-        } else if (boxes[0].innerText === player.innerText && boxes[3].innerText === player.innerText && boxes[6].innerText === player.innerText) {
-            winMsg.innerText = `Player ${player.innerText} wins`;
-        } else if (boxes[1].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[7].innerText === player.innerText) {
-            winMsg.innerText = `Player ${player.innerText} wins`;
-        } else if (boxes[2].innerText === player.innerText && boxes[5].innerText === player.innerText && boxes[8].innerText === player.innerText) {
-            winMsg.innerText = `Player ${player.innerText} wins`;
-        }
+            if (boxes[0].innerText === player.innerText && boxes[1].innerText === player.innerText && boxes[2].innerText === player.innerText){
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[0].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[8].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[0].innerText === player.innerText && boxes[3].innerText === player.innerText && boxes[6].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[1].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[7].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[2].innerText === player.innerText && boxes[5].innerText === player.innerText && boxes[8].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[3].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[5].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[6].innerText === player.innerText && boxes[7].innerText === player.innerText && boxes[9].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            } else if (boxes[2].innerText === player.innerText && boxes[4].innerText === player.innerText && boxes[6].innerText === player.innerText) {
+                winMsg.innerText = `Player ${player.innerText} wins`;
+            }
         })
     }
 })
